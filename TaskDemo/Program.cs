7e0ad77dabbb484de41ed36e18b5a8d6a0850e4d
Roman Satanovskyi <Roman.Satanovskyi@mindgeek.com>
2020-01-07 16:20:01 +0200

    TASK DEMO

---
+++ TaskDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TaskDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var d = MyMethod(12);
+            //Console.WriteLine("Hello World");
+            Console.WriteLine(d.Result);
+            
+            //Console.WriteLine(111);
+        }
+        public static async Task<int> MyMethod(int myParameter)
+        {
+            return 123;
+        }
+    }
+}
